358c91cd35c93498108189f95fc27776368395a9
Edward Koetsjarjan <edward.koetsjarjan@finalist.nl>
2025-09-04 00:36:06 +0200

    subscribe

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
+"use dom";
+import { app, db } from "@/firebaseConfig";
 import { Link } from "expo-router";
-import React from "react";
-import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+import React, { useEffect, useState, useRef } from "react";
+import { Text, View, Image, StyleSheet, ScrollView, Alert } from "react-native";
+import * as Device from "expo-device";
 const logo = require("../../assets/logo/FullLogo_Transparent.png");
 
-export default function Page() {
+export default function Home() {
+  useEffect(() => {}, []);
+
   return (
-    <View className="flex flex-1">
+    <View style={styles.container}>
       {/* <Header /> */}
       <Content />
-      {/* <Footer /> */}
+      <Footer />
     </View>
   );
 }
@@ -25,6 +30,54 @@ function Content() {
     >
       <Image source={logo} style={{ width: "100%" }} resizeMode="center" />
     </ScrollView>
+  );
+}
+
+function Footer() {
+  const emailRef = useRef(null);
+  const [subscribed, setsubscribed] = useState(false);
+  const onSubscribe = () => {
+    const email = emailRef.current.value;
+    if (email != "" && email.includes("@"))
+      setDoc(doc(db, "subscribe", email), {
+        info: `${Device.deviceName ? Device.deviceName + ", " : ""}${
+          Device.modelName
+        }, ${Device.osName}`,
+        timetamp: Timestamp.now(),
+      }).then(() => {
+        setsubscribed(true);
+      });
+  };
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: 50,
+      }}
+    >
+      <form className="max-w-md mx-auto">
+        {subscribed ? (
+          <h1 className="text-5xl font-bold mb-8 animate-pulse">Thank you</h1>
+        ) : (
+          <div className="flex items-center">
+            <input
+              type="email"
+              className="bg-gray-100 mr-3 py-2 px-4 w-full rounded-md focus:outline-none focus:bg-white"
+              placeholder="Enter your email"
+              ref={emailRef}
+            />
+            <button
+              type="button"
+              className="bg-purple-600 text-white py-2 px-4 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
+              onClick={onSubscribe}
+            >
+              Subscribe
+            </button>
+          </div>
+        )}
+      </form>
+    </View>
   );
 }
 
